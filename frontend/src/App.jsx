3dc9fd5b4589a23e9e9a86f0cbc593ca8809a713
Anushka04081 <anushkabachkheti@gmail.com>
2026-07-13 16:56:49 +0530

    Initialize React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <nav>
+        <h2>ResumeFit</h2>
+      </nav>
+
+      <section className="hero">
+        <h1>Analyze Your Resume with AI</h1>
+
+        <p>
+          Get instant feedback, ATS score, and personalized suggestions
+          to improve your resume.
+        </p>
+
+        <button>Upload Resume</button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
